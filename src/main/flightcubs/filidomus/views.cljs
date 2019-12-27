5bd2da91792903fd4f1586d6f621ebc9e28ccd1f
Filip Hedberg <flightcubs@gmail.com>
2019-12-27 11:36:39 +0100

    Add simple blog list

--- src/main/flightcubs/filidomus/views.cljs
+++ src/main/flightcubs/filidomus/views.cljs
@@ -8,34 +8,49 @@
 
 (defn home-page []
   [:div
-   [:h1 "Hello world!"]
-   [:p "test"]
-   [:> react-markdown {:source md-test}]
-   ])
+   [:h1 "Hello world!"]])
+
+(def posts
+  {:hello-blog      {:name     "Hello blog!"
+                     :subtitle "Everything starts with a purpose"
+                     :date     "2019-12-27"
+                     :markdown (rc/inline "./posts/test.md")}
+   :building-a-blog {:name     "Building this blog"
+                     :date     "2019-12-29"
+                     :markdown (rc/inline "./posts/test.md")}
+   :first-post      {:name     "First post"
+                     :date     "2019-11-29"
+                     :markdown (rc/inline "./posts/test.md")}
+   })
+
+(defn posts-by-date [posts]
+  (sort-by (fn [[slug post]] (:date post)) posts))
+
+(defn future-date? [iso-date]
+  (> (.parse js/Date iso-date) (.now js/Date)))
+
+(defn show-post [[slug post]]
+  (not (future-date? (:date post))))
+
+(defn blog-post-link [slug post]
+  [:a {:href (str "/#/blog/post/" (name slug))}
+   (str (:date post) " - " (:name post))])
 
 (defn blog []
   [:div
-   [:h1 "Blog"]])
-
-
-(def posts
-  {:my-first-post {:name     "My first post"
-                   :date     "2019-12-26"
-                   :markdown (rc/inline "./posts/test.md")}})
-
+   [:h1 "Blog"]
+   (for [[slug post] (filter show-post (posts-by-date posts))]
+     [:div
+      [blog-post-link slug post]])])
 
 (defn blog-post []
-  (let [content (->> (<sub [:path-params])
-                     :slug
-                     keyword
-                     (get posts)
-                     :markdown)]
+  (let [content (->> (<sub [:path-params]) :slug keyword (get posts) :markdown)]
     [:div
-     [:> react-markdown content]
-     ]))
+     [:> react-markdown content]]))
 
 (defn main-view []
   (let [current-route (<sub [:current-route])]
     [:div
      (when current-route
        [(-> current-route :data :view)])]))
+
